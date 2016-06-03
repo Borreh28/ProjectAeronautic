9f0fff6b27172795763b80561fb10e2178ca29c4
Borreh28 <borreh.2814@gmail.com>
2016-06-03 00:34:40 -0700

    Format added, without edit and delete lines functionality

---
+++ App/App/Scripts/editRow.js
@@ -0,0 +1,14 @@
+﻿$(".getRow").click(function (e) {
+    e.preventDefault();
+    $("#lineaId").val(($(this).attr('getLineaId')));
+    $("#reqId").val(($(this).attr('getReqId')));
+    $("#linea").val(($(this).attr('getLinea')));
+    $("#parteId").val(($(this).attr('getParteId')));
+    $("#Cantidad").val(($(this).attr('getCantidad')));
+    $("#precioVenta").val(($(this).attr('getPrecioVenta')));
+    $("#descripcion").val(($(this).attr('getDescripcion')));
+    $("#creadoPor").val(($(this).attr('getCreadoPor')));
+    $("#creado").val(($(this).attr('getCreado')));
+    $("#actualizadoPor").val(($(this).attr('getActualizadoPor')));
+    $("#actualizado").val(($(this).attr('getActualizado')));
+});
